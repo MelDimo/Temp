ddf36b90c791fcf51e9535a7a058182e71eba9f5
Dimon <meldimo@gmail.com>
2018-06-25 18:30:38 +0300

    from work

---
+++ ChangeDataEvent/BaseEvent/BaseEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseEvent
+{
+    public class EventParam
+    {
+        public string ModuleNameFrom { get; set; }
+        public string ModuleNameTo { get; set; } // When empty - for all
+        public object[] Param { get; set; }
+    }
+
+    public sealed class ReferenceChanges
+    {
+        public delegate void RefChanged(EventParam param);
+        public event RefChanged OnReferenceChanged;
+
+        private ReferenceChanges()
+        {
+            
+        }
+
+        private static readonly Lazy<ReferenceChanges> lazy = new Lazy<ReferenceChanges>(() => new ReferenceChanges());
+
+        public static ReferenceChanges GetInstance { get { return lazy.Value; } }
+
+        public void ReferenceChanged(EventParam param)
+        {
+            OnReferenceChanged(param);
+        }
+
+    }
+
+
+}
---
+++ ChangeDataEvent/References/ViewModel/Unit/UnitViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseEvent;
+
+namespace References.ViewModel.Unit
+{
+    public class UnitViewModel
+    {
+        ReferenceChanges referenceChanges;
+
+        public UnitViewModel()
+        {
+            referenceChanges = ReferenceChanges.GetInstance;
+            referenceChanges.OnReferenceChanged += ReferenceChanges_OnReferenceChanged;
+            referenceChanges.ReferenceChanged(new EventParam()
+            {
+                ModuleNameFrom = "UnitViewModel",
+                ModuleNameTo = string.Empty,
+                Param = new object[] { }
+            });
+        }
+
+        private void ReferenceChanges_OnReferenceChanged(EventParam param)
+        {
+            Debug.Print($"References.ViewModel.Unit.UnitViewModel recive changed: {param.ModuleNameTo}");
+        }
+
+        //ReferenceChanges
+
+    }
+}
